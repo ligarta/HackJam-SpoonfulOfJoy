269429834a5cb35ec072f90b4ca0f8324b530cc1
M. Fahrezel Ravie Iskandar(235150200111026) <mfahraviisk@gmail.com>
2025-09-06 01:54:18 +0700

    capekk

--- Assets/Script/mainMenuManager.cs
+++ Assets/Script/mainMenuManager.cs
@@ -1,4 +1,3 @@
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -13,49 +12,80 @@ public class mainMenuManager : MonoBehaviour
     [SerializeField] AudioClip SwipeSound;
     [SerializeField] AudioClip OpenDoor;
     [SerializeField] GameObject CreditPanel;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void Start()
-    {
-    }
-    void Update()
-    {
-
-    }
     public void gameStart()
     {
         clickAudio.PlayOneShot(SwipeSound);
-        LeanTween.moveX(mainMenu, -1920f, 0.8f).setEaseInExpo().setOnComplete(() =>
-        {
-            mainMenu.SetActive(false);
-        });
+
+        // Gunakan GetComponent<RectTransform>() langsung
+        LeanTween.moveX(mainMenu.GetComponent<RectTransform>(),
+            mainMenu.GetComponent<RectTransform>().anchoredPosition.x - Screen.width, 0.8f)
+            .setEaseInExpo()
+            .setOnComplete(() =>
+            {
+                mainMenu.SetActive(false);
+            });
+
+        // Panel fade out
         LeanTween.alpha(Panel.GetComponent<RectTransform>(), 0f, 0.8f).setEaseInExpo();
+
+        // Door sound effect
         LeanTween.delayedCall(1f, () =>
         {
             clickAudio.PlayOneShot(OpenDoor);
         });
+
+        // Panel movements using current positions
         LeanTween.moveX(RightPanel, 50f, 2f).setEaseInOutQuad().setDelay(1f);
         LeanTween.moveX(LeftPanel, -50f, 2f).setEaseInOutQuad().setDelay(1f);
+
+        // Transition fade
         LeanTween.alpha(TransitionPanel, 1f, 2f).setEaseInOutExpo().setDelay(0.3f).setOnComplete(() =>
-        {
-            LeanTween.delayedCall(1.5f, () =>
+         {
+             LeanTween.delayedCall(1.5f, () =>
+             {
+                 SceneManager.LoadScene("IntroScene");
+             });
+         });
+    }
+    float temp = 0f;
+
+    public void toCredit()
+    {
+        clickAudio.PlayOneShot(SwipeSound);
+
+        CreditPanel.SetActive(true);
+        temp = mainMenu.GetComponent<RectTransform>().anchoredPosition.x;
+        LeanTween.moveX(mainMenu.GetComponent<RectTransform>(),
+            mainMenu.GetComponent<RectTransform>().anchoredPosition.x - Screen.width, 0.8f)
+            .setEaseInOutExpo()
+            .setOnComplete(() =>
             {
-                SceneManager.LoadScene("IntroScene");
+                mainMenu.SetActive(false);
             });
-        });
+
+        LeanTween.moveX(CreditPanel.GetComponent<RectTransform>(),
+            0, 0.8f)
+            .setEaseInOutExpo();
+    }
+
+    public void closeCredit()
+    {
+        mainMenu.SetActive(true);
+        clickAudio.PlayOneShot(SwipeSound);
+        LeanTween.moveX(mainMenu.GetComponent<RectTransform>(), temp, 0.8f)
+            .setEaseInOutExpo();
+
+        LeanTween.moveX(CreditPanel.GetComponent<RectTransform>(),
+            Screen.width, 0.8f)
+            .setEaseInOutExpo().setOnComplete(() =>
+            {
+                CreditPanel.SetActive(false);
+            });
     }
 
     public void quitGame()
     {
         Application.Quit();
-    }
-
-    public void toCredit()
-    {
-        CreditPanel.SetActive(true);
-    }
-    public void closeCredit()
-    {
-        CreditPanel.SetActive(false);
     }
 }
